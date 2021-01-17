4650ff21d9ffd68f9a5c0d6dc5070557ba390116
Baran Köse <barankose@hotmail.com>
2021-01-17 19:05:47 +0300

    Ödev3

---
+++ CustomerManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer musteri)
+        {
+            Console.WriteLine("Müşteri Bilgileri Eklendi: " + musteri.Ad);
+            Console.WriteLine("Müşteri Bilgileri Eklendi: " + musteri.Soyad);
+            Console.WriteLine("Müşteri Bilgileri Eklendi: " + musteri.MusteriNo);
+
+        }
+
+        public void Remove(Customer musteri)
+        {
+            Console.WriteLine("Müşteri Bilgileri Çıkarıldı: " + musteri.Ad );
+            Console.WriteLine("Müşteri Bilgileri Çıkarıldı: " + musteri.Soyad );
+            Console.WriteLine("Müşteri Bilgileri Çıkarıldı: " + musteri.MusteriNo );
+        }
+        public void List(Customer musteriler)
+        {
+            Console.WriteLine("Müşterilerin Numaraları: " + musteriler.MusteriNo);
+        }
+
+        public void Add2(string musteriAdi, string soyAdi, int mstNo)
+        {
+            Console.WriteLine("Müşteri Adı Eklendi : " + musteriAdi);
+            Console.WriteLine("Müşteri Soyadı Eklendi : " + soyAdi);
+            Console.WriteLine("Müşteri No Eklendi : " + mstNo);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Customer musteri1 = new Customer();
+            musteri1.Ad = "Baran";
+            musteri1.Soyad = "Köse";
+            musteri1.MusteriNo = 4441;
+
+            Customer musteri2 = new Customer();
+            musteri2.Ad = "Engin";
+            musteri2.Soyad = "Demirog";
+            musteri2.MusteriNo = 2021;
+
+            Customer[] musteriler = new Customer[] { musteri1, musteri2 };
+
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Ad);
+                Console.WriteLine(musteri.Soyad);
+                Console.WriteLine(musteri.MusteriNo);
+                Console.WriteLine("^^^^^^^^^^^^^^^^^^^^");
+
+
+            }
+
+            Console.WriteLine("Durum");
+
+            CustomerManager musterilist = new CustomerManager();
+            musterilist.List(musteri1);
+            musterilist.List(musteri2);
+
+            Console.WriteLine("-----------------");
+            CustomerManager musteriekleme = new CustomerManager();
+            musteriekleme.Add(musteri1);
+
+            CustomerManager mustericıkarma = new CustomerManager();
+            Console.WriteLine("----------------------");
+            musteriekleme.Remove(musteri2);
+
+            Console.WriteLine("-----------------");
+            musterilist.Add2("Onur","Sancay",1234);
+
+
+            Console.WriteLine("Ödev 3'ün sonu");
+            Console.ReadLine();
+
+            
+
+
+
+
+        }
+    }
+}
